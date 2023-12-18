298bb6b097b36a39b3f4704acf7bb69e2f5512e8
unknown <omarelkady171@gmail.com>
2023-12-18 18:14:56 +0200

    lollplllll

--- firstgithub.cpp
+++ firstgithub.cpp
@@ -6,8 +6,12 @@ int main()
     int n;
 cout<<"first github"<<endl;
 cout<<"for branch"<<endl;
-cout<<"ya moshelll"<<endl;
+
 cin>>n;
 cout <<n<<endl;
+if(n==3)
+{
+    cout<<"well well well"<<endl;
+}
 }
 
